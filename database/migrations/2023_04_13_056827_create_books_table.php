d7f8c5b1aeba134d47a70436a6cae86763c5edd1
Parham <Parham.parnian@gmail.com>
2023-04-17 00:44:31 +0330

    migrations added

---
+++ database/migrations/2023_04_13_056827_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('summary');
+            $table->string('publisher');
+            $table->timestamps();
+
+            $table->fullText('title')->language('english');	
+            $table->fullText('summary')->language('english');	
+            $table->fullText('publisher')->language('english');	
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
